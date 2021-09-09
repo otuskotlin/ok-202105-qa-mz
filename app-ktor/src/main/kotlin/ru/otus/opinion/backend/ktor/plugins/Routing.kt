1d0cb02e32802326fcc585e90e34ac9919017fdf
Mikhail Zvagelsky <mikhailzvagelsky@gmail.com>
2021-09-09 23:14:45 +0300

    Extract QuestionController interface.

--- app-ktor/src/main/kotlin/ru/otus/opinion/backend/ktor/plugins/Routing.kt
+++ app-ktor/src/main/kotlin/ru/otus/opinion/backend/ktor/plugins/Routing.kt
@@ -4,13 +4,14 @@ import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import ru.otus.opinion.backend.ktor.controllers.QuestionController
+import ru.otus.opinion.backend.ktor.controllers.QuestionControllerImpl
 import ru.otus.opinion.bakend.services.QuestionService
 import ru.otus.opinion.bakend.services.QuestionServiceImpl
 
 fun Application.configureRouting() {
 
     val questionService: QuestionService = QuestionServiceImpl()
-    val questionController = QuestionController(questionService)
+    val questionController: QuestionController = QuestionControllerImpl(questionService)
 
     routing {
         get("/") {
