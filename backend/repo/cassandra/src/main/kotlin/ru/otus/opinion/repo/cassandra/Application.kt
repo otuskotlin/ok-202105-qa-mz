412c6c91c07fdf6407f17d4e286d0e6e90d7bd23
Mikhail Zvagelsky <mikhailzvagelsky@gmail.com>
2021-11-18 20:27:10 +0300

    Remove main() from Cassandra repository.

--- backend/repo/cassandra/src/main/kotlin/ru/otus/opinion/repo/cassandra/Application.kt
+++
@@ -1,8 +0,0 @@
-package ru.otus.opinion.repo.cassandra
-
-import ru.otus.opinion.repo.cassandra.schema.SchemaInitializer
-
-fun main() {
-    val builder = SchemaInitializer()
-    builder.init()
-}
